42226ec7341e3a5bdd3afe60222c650db47c48ef
Ayşegül Demirtaş <aysegul_demirtas@hotmail.com.tr>
2018-11-17 23:52:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ def main():
    # print sys.argv[1]
     print "Train Baslatildi \n"
 
-
-
     img = cv2.imread("c.jpg")
-
 
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
 
     cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
